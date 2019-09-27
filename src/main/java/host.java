686c63311a9dd4ec96459ff056cd0973f62ae1f0
Wasser <5126035+ansuztrent@user.noreply.gitee.com>
2019-09-27 23:02:49 +0800

    !2 多线程完成，合并 * 完成：多线程 * 优化代码结构 * 修复查询功能 * 正则修复，感谢Lunex 和Q.E.D 的帮助 * 多线程筹备中 * GUI 完成 * GUI 完成 * GUI 分离 * 初探jframe * 修复：更新hosts 时备份文件操作多次执行 * 修复：无限弹窗制 * 新增：图形化界面草创

--- src/main/java/host.java
+++ src/main/java/host.java
@@ -6,104 +6,88 @@ import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 public class host {
+
 	private static String DesktopPath = FileSystemView.getFileSystemView().getHomeDirectory().getPath();
 
-	host() {
-		/*
-	Document中提供了丰富的方法来获取指定元素。
+	public static void main(String[] args) {
+//		Menu();
+		new GUI();
+	}
 
-	◇使用DOM的方式来取得
-		getElementById(String id)：通过id来获取
-			getElementsByTag(String tagName)：通过标签名字来获取
-　			getElementsByClass(String className)：通过类名来获取
-　			getElementsByAttribute(String key)：通过属性名字来获取
-　			getElementsByAttributeValue(String key, String value)：通过指定的属性名字，属性值来获取
-　			getAllElements()：获取所有元素
+	private static Boolean Backup() {
+		File hosts = new File("C:\\Windows\\System32\\drivers\\etc\\hosts");
+		//备份hosts
+		File backup = new File(DesktopPath + "\\hosts.bak");
+		try {
+			if (backup.exists())
+				Files.delete(backup.toPath());
+			Files.copy(hosts.toPath(), backup.toPath());
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	◇通过类似于css或jQuery的选择器来查找元素
+		return false;
+	}
 
-		使用的是Element类的下记方法：
+	private static void AppendNew(String str) {
+		Vector<String> recode = ReadPage(str);
+		try {
+			Files.copy(new File("C:\\Windows\\System32\\drivers\\etc\\hosts").toPath(), new File(DesktopPath + "\\hosts").toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (!recode.isEmpty() && Backup())
+			Append(recode);
+		OpenEtc();
+	}
 
-			public Elements select(String cssQuery)
+	private static void Update() {
+		Vector<String> urls = Objects.requireNonNull(ReadHosts());
+		if (!urls.isEmpty() && Backup()) {
+			try {
+				FileWriter fileWriter = new FileWriter(DesktopPath + "\\hosts");
+				fileWriter.write(proString());
+				fileWriter.flush();
+				fileWriter.close();
 
-		通过传入一个类似于CSS或jQuery的选择器字符串，来查找指定元素。
+				//设定线程池
+				ExecutorService pool = Executors.newFixedThreadPool(8);
+				for (int i = 0; i < urls.size(); i++) {
+					int finalI = i;
+					pool.execute(new Thread(() -> {
+						System.out.println(urls.elementAt(finalI));
+						Append(ReadPage(urls.elementAt(finalI)));
+					}));
+				}
+				pool.shutdown();
+				while (true)
+					if (pool.isTerminated())
+						break;
 
-		例子：
-
-			File input = new File("/tmp/input.html");
-			Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
-
-			Elements links = doc.select("a[href]"); //带有href属性的a元素
-			Elements pngs = doc.select("img[src$=.png]");
-			//扩展名为.png的图片
-
-			Element masthead = doc.select("div.masthead").first();
-			//class等于masthead的div标签
-
-			Elements resultLinks = doc.select("h3.r > a"); //在h3元素之后的a元素
-
-
-	选择器的更多语法(可以在org.jsoup.select.Selector中查看到更多关于选择器的语法)：
-
-	tagname: 通过标签查找元素，比如：a
-	　　ns|tag: 通过标签在命名空间查找元素，比如：可以用 fb|name 语法来查找 <fb:name> 元素
-	　　#id: 通过ID查找元素，比如：#logo
-		.class: 通过class名称查找元素，比如：.masthead
-		[attribute]: 利用属性查找元素，比如：[href]
-		[^attr]: 利用属性名前缀来查找元素，比如：可以用[^data-] 来查找带有HTML5 Dataset属性的元素
-		[attr=value]: 利用属性值来查找元素，比如：[width=500]
-		[attr^=value], [attr$=value], [attr=value]: 利用匹配属性值开头、结尾或包含属性值来查找元素，比如：[href=/path/]
-		[attr~=regex]: 利用属性值匹配正则表达式来查找元素，比如： img[src~=(?i).(png|jpe?g)]
-		*: 这个符号将匹配所有元素
-
-	Selector选择器组合使用
-　		el#id: 元素+ID，比如： div#logo
-		el.class: 元素+class，比如： div.masthead
-		el[attr]: 元素+class，比如： a[href]
-		任意组合，比如：a[href].highlight
-　		ancestor child: 查找某个元素下子元素，比如：可以用.body p 查找在"body"元素下的所有 p元素
-　		parent > child: 查找某个父元素下的直接子元素，比如：可以用div.content > p 查找 p 元素，也可以用body > * 查找body标签下所有直接子元素
-　		siblingA + siblingB: 查找在A元素之前第一个同级元素B，比如：div.head + div
-　		siblingA ~ siblingX: 查找A元素之前的同级X元素，比如：h1 ~ p
-　		el, el, el:多个选择器组合，查找匹配任一选择器的唯一元素，例如：div.masthead, div.logo
-	伪选择器selectors
-　		:lt(n): 查找哪些元素的同级索引值（它的位置在DOM树中是相对于它的父节点）小于n，比如：td:lt(3) 表示小于三列的元素
-　		:gt(n):查找哪些元素的同级索引值大于n，比如： div p:gt(2)表示哪些div中有包含2个以上的p元素
-　		:eq(n): 查找哪些元素的同级索引值与n相等，比如：form input:eq(1)表示包含一个input标签的Form元素
-　		:has(seletor): 查找匹配选择器包含元素的元素，比如：div:has(p)表示哪些div包含了p元素
-　		:not(selector): 查找与选择器不匹配的元素，比如： div:not(.logo) 表示不包含 class="logo" 元素的所有 div 列表
-　		:contains(text): 查找包含给定文本的元素，搜索不区分大不写，比如： p:contains(jsoup)
-　		:containsOwn(text): 查找直接包含给定文本的元素
-　		:matches(regex): 查找哪些元素的文本匹配指定的正则表达式，比如：div:matches((?i)login)
-　		:matchesOwn(regex): 查找自身包含文本匹配指定正则表达式的元素
-	注意　：上述伪选择器索引是从0开始的，也就是说第一个元素索引值为0，第二个元素index为1等
-
-	◆通过上面的选择器，我们可以取得一个Elements对象，它继承了ArrayList对象，里面放的全是Element对象。
-		接下来我们要做的就是从Element对象中，取出我们真正需要的内容。
-
-		通常有下面几种方法：
-
-			◇Element.text()
-			这个方法用来取得一个元素中的文本。
-
-			◇Element.html()或Node.outerHtml()
-			这个方法用来取得一个元素中的html内容
-
-			◇Node.attr(String key)
-			获得一个属性的值，例如取得超链接<a href="">中href的值
-
-*/
+				OpenEtc();
+				//移动，但目前不能获取管理员权限写入C 盘
+//				Files.move(bak1.toPath(), hosts.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private static String proString() {
@@ -131,61 +115,15 @@ public class host {
 				"\n";
 	}
 
-	private static Boolean Append(Vector<String> recode) {
-		if (!recode.isEmpty() && Backup()) {
-			try {
-				FileWriter fileWriter1 = new FileWriter(DesktopPath + "\\hosts", true);
-				for (int i = 0; i < recode.size(); i++)
-					fileWriter1.write(recode.elementAt(i));
-				fileWriter1.close();
-				return true;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-
-	private static Boolean UpdateHosts(Vector<String> urls) {
-		if (!urls.isEmpty() && Backup()) {
-			try {
-				FileWriter fileWriter = new FileWriter(DesktopPath + "\\hosts");
-				fileWriter.write(proString());
-				fileWriter.flush();
-				fileWriter.close();
-
-				for (int i = 0; i < urls.size(); i++)
-					Append(ReadPage(urls.elementAt(i)));
-
-				//移动，但目前不能获取管理员权限写入C 盘
-//				Files.move(bak1.toPath(), hosts.toPath());
-
-				return true;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
-
-	private static Boolean Backup() {
-		File hosts = new File("C:\\Windows\\System32\\drivers\\etc\\hosts");
-		//备份hosts
-		File backup = new File(DesktopPath + "\\hosts.bak");
+	private static void OpenEtc() {
 		try {
-			if (backup.exists())
-				Files.delete(backup.toPath());
-			Files.copy(hosts.toPath(), backup.toPath());
-			return true;
+			Desktop.getDesktop().open(new File("C:\\Windows\\System32\\drivers\\etc"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		return false;
 	}
 
 	private static Document getDocumentFromPage(String url) throws IOException {
-		System.out.println("Page loading...");
 		//不打印日志
 		LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
 		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
@@ -209,16 +147,7 @@ public class host {
 		//等待后台运行
 		webClient.waitForBackgroundJavaScript(10 * 1000);
 
-		System.out.println("Loaded.");
 		return Jsoup.parse(page.asXml(), url);
-	}
-
-	private static void OpenEtc() {
-		try {
-			Desktop.getDesktop().open(new File("C:\\Windows\\System32\\drivers\\etc\\"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	private static Vector<String> ReadPage(String url) {
@@ -232,17 +161,15 @@ public class host {
 		try {
 			Document doc = getDocumentFromPage(AimURL);
 
-			System.out.println("The string is dealing...");
 			String host = doc.getElementById("host").attr("value");
 
-			String[] IPTmp = doc.getElementsByClass("w60-0 tl").text().split(" ");
+			String[] IPTmp = doc.getElementsByClass("w60-0 tl").text().split("\\[.*?]");
 			String[] IP = new String[IPTmp.length];
 			for (int i = 0, j = 0; i < IPTmp.length; i++) {
-				IPTmp[i] = IPTmp[i].replaceAll("[^\\d{1,3}.]", "").replaceAll("\\.\\.+", "");
+				IPTmp[i] = IPTmp[i].replaceAll("([ \\-]|\\.\\.+)", "");
 				if (IPTmp[i].equals(""))
 					continue;
-				IP[j] = IPTmp[i];
-				j++;
+				IP[j++] = IPTmp[i];
 			}
 
 //			String[] str = doc.getElementsByClass("w14-0").text().split(" ");
@@ -254,24 +181,23 @@ public class host {
 //				j++;
 //			}
 
-			Vector<String> tmp = new Vector<>();
+//			Vector<String> tmp = new Vector<>();
 //			for (int i = 0; i < Math.min(TTL.length, IP.length); i++)
 //				if (!(IP[i] == null))
 //					if (tmp.indexOf(IP[i]) == -1 & !IP[i].equals("-"))
 //						tmp.addElement(IP[i] + " " + TTL[i]);
 			for (String s : IP) {
 				if (!(s == null))
-					if (tmp.indexOf(s + " " + host) == -1 & !s.equals("-"))
-						tmp.addElement(s + " " + host);
+					if (recode.indexOf("\n" + s + " " + host) == -1 & !s.equals("-"))
+						recode.addElement("\n" + s + " " + host);
 			}
 
-			Collections.sort(tmp);
-			for (int i = 0; i < tmp.size(); i++)
-				recode.addElement("\n" + tmp.elementAt(i));
+			Collections.sort(recode);
+//			for (int i = 0; i < tmp.size(); i++)
+//				recode.addElement("\n" + tmp.elementAt(i));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Finish");
 		recode.addElement("\n");
 		return recode;
 	}
@@ -308,41 +234,123 @@ public class host {
 		return recode.isEmpty() ? null : recode;
 	}
 
-	private static void Menu() {
-		//hosts 备份位于桌面
-		Scanner sc = new Scanner(System.in);
-		String s = "";
-		while (!s.equals("quit")) {
-			Boolean flag = false;
-			System.out.println("1 更新hosts\n" + "2 新增URL\n" + "3 备份hosts\n" + "输入quit 退出");
-			s = sc.nextLine();
-			switch (s) {
-				case "1":
-					flag = UpdateHosts(Objects.requireNonNull(ReadHosts()));
-					OpenEtc();
-					break;
-				case "2":
-					System.out.println("Input the URL:");
-					flag = Append(ReadPage(sc.next()));
-					OpenEtc();
-					break;
-				case "3":
-					flag = Backup();
-					break;
-			}
-			if (flag)
-				break;
-			else
-				System.out.println("请重试");
+	private static void Append(Vector<String> recode) {
+		try {
+			FileWriter fileWriter1 = new FileWriter(DesktopPath + "\\hosts", true);
+			for (int i = 0; i < recode.size(); i++)
+				fileWriter1.write(recode.elementAt(i));
+			fileWriter1.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static void GUI() {
-
+	private static void Menu() {
+		//hosts 备份位于桌面
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		while (flag) {
+			flag = false;
+			System.out.println("1 更新hosts\n" + "2 新增URL\n" + "3 备份hosts\t" + "输入quit 退出");
+			String s = sc.nextLine();
+			switch (s) {
+				case "1":
+					Update();
+					break;
+				case "2":
+					System.out.println("Input the URL:");
+					AppendNew(sc.next());
+					break;
+				case "3":
+					Backup();
+					break;
+				default:
+					if (!s.equals("quit")) {
+						System.out.println("请重试");
+						flag = true;
+					}
+			}
+		}
 	}
 
-	public static void main(String[] args) {
-		host.Menu();
-		GUI();
+	static class GUI extends JFrame implements ActionListener {
+		private JButton updateHosts, search, backupHosts;
+		private JTextField hosts;
+
+		GUI() {
+			//左侧栏
+			JTextArea textArea = new JTextArea("广告位招租");
+			//设置只读
+			textArea.setEditable(false);
+			JPanel update = new JPanel();
+			update.add(textArea);
+			add(update, BorderLayout.WEST);
+
+			//中栏
+			JTextArea textA = new JTextArea("到时候输出记录");
+			//设置只读
+			textA.setEditable(false);
+			JPanel recode = new JPanel();
+			recode.setLayout(new GridLayout(1, 1));
+			recode.add(textA);
+			add(recode, BorderLayout.CENTER);
+
+			//顶栏
+			hosts = new JTextField();
+			search = new JButton("搜索");
+			search.addActionListener(this);
+			JPanel append = new JPanel();
+			append.setLayout(new GridLayout(1, 2));
+			append.add(hosts);
+			append.add(search);
+			add(append, BorderLayout.NORTH);
+
+			//底栏
+			backupHosts = new JButton("备份");
+			backupHosts.addActionListener(this);
+			updateHosts = new JButton("更新");
+			updateHosts.addActionListener(this);
+			JPanel backup = new JPanel();
+			backup.setLayout(new GridLayout(1, 2));
+			backup.add(backupHosts);
+			backup.add(updateHosts);
+			add(backup, BorderLayout.SOUTH);
+
+			setTitle("test");
+			//大小
+			setSize(500, 500);
+			//是否可改变大小
+//			setResizable(false);
+			//出现位置居中
+			setLocationRelativeTo(null);
+//			setLocation(1200, 200);
+			//关闭窗口按钮
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			//是否可见
+			setVisible(true);
+		}
+
+		private static void setButtonStatus(boolean f, JButton... a) {
+			for (JButton button : a)
+				button.setEnabled(f);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == search) {
+				setButtonStatus(false, backupHosts, updateHosts);
+				host.AppendNew(hosts.getText());
+				setButtonStatus(true, backupHosts, updateHosts);
+			} else if (e.getSource() == backupHosts) {
+				setButtonStatus(false, search, updateHosts);
+				host.Backup();
+				setButtonStatus(true, search, updateHosts);
+			} else {
+				setButtonStatus(false, search, backupHosts);
+				host.Update();
+				setButtonStatus(true, search, backupHosts);
+			}
+		}
 	}
+
 }
--- src/main/kotlin/host.kt
+++ src/main/kotlin/host.kt
@@ -4,42 +4,63 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage
 import org.apache.commons.logging.LogFactory
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.awt.BorderLayout
 import java.awt.Desktop
+import java.awt.GridLayout
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 import java.io.File
-import java.io.FileWriter
 import java.io.IOException
 import java.nio.file.Files
 import java.util.*
+import java.util.concurrent.Executors
 import java.util.logging.Level
+import javax.swing.*
 import javax.swing.filechooser.FileSystemView
 
-private val DesktopPath = FileSystemView.getFileSystemView().homeDirectory.path
-
-private val proString: () -> String = {
-	"# Copyright (c) 1993-2009 Microsoft Corp.\n" + "#\n" +
-			"# This is getDocumentFromPage sample HOSTS file used by Microsoft TCP/IP for Windows.\n" + "#\n" +
-			"# This file contains the mappings of IP addresses to host names. Each\n" +
-			"# entry should be kept on an individual line. The IP address should\n" +
-			"# be placed in the first column followed by the corresponding host name.\n" +
-			"# The IP address and the host name should be separated by at least one\n" +
-			"# space.\n" + "#\n" +
-			"# Additionally, comments (such as these) may be inserted on individual\n" +
-			"# lines or following the machine name denoted by getDocumentFromPage '#' symbol.\n" + "#\n" +
-			"# For example:\n" + "#\n" +
-			"#      102.54.94.97     rhino.acme.com          # source server\n" +
-			"#       38.25.63.10     x.acme.com              # x client host\n" + "\n" +
-			"# localhost name resolution is handled within DNS itself.\n" +
-			"#\t127.0.0.1       localhost\n" +
-			"#\t::1             localhost\n" + "\n"
+fun main() {
+//	menu()
+	GUI()
 }
 
-private val openEtc: () -> Unit = { Desktop.getDesktop().open(File("C:\\Windows\\System32\\drivers\\etc\\")) }
+fun update() {
+	val urls = Objects.requireNonNull<Vector<String>>(readHosts())
+	if (!urls.isEmpty() && backup()) {
+		val fileWriter = File("$DesktopPath\\hosts")
+		fileWriter.writeText(proString())
+
+		//设定线程池
+		val pool = Executors.newFixedThreadPool(8)
+		for (i in urls.indices)
+			pool.execute(Thread {
+				println(urls.elementAt(i))
+
+				append(readPage(urls.elementAt(i)))
+			})
+		pool.shutdown()
+		while (true)
+			if (pool.isTerminated)
+				break
+
+		openEtc()
+		//移动，但目前不能获取管理员权限写入C 盘
+//		Files.move(bak1.toPath(), hosts.toPath());
+	}
+}
+
+private fun appendNew(str: String) {
+	val recode = readPage(str)
+	Files.copy(File("$path\\hosts").toPath(), File("$DesktopPath\\hosts").toPath())
+	if (!recode.isEmpty() && backup())
+		append(recode)
+	openEtc()
+}
 
 private fun backup(): Boolean {
-	val hosts = File("C:\\Windows\\System32\\drivers\\etc\\hosts")
-	//备份hosts
-	val backup = File("$DesktopPath\\hosts.bak")
 	try {
+		val hosts = File("$path\\hosts")
+		//备份hosts
+		val backup = File("$DesktopPath\\hosts.bak")
 		if (backup.exists())
 			Files.delete(backup.toPath())
 		Files.copy(hosts.toPath(), backup.toPath())
@@ -50,82 +71,40 @@ private fun backup(): Boolean {
 	return false
 }
 
-private fun readPage(url: String): Vector<String> {
-	val aimURL = "http://tool.chinaz.com/dns?type=1&host=$url&ip="
-	//设置代理
-//	System.setProperty("http.proxyHost", "127.0.0.1")
-//	System.setProperty("http.proxyPort", "8090")
+private val DesktopPath = FileSystemView.getFileSystemView().homeDirectory.path
 
-	val recode = Vector<String>()
+private const val path = "C:\\Windows\\System32\\drivers\\etc"
 
-	try {
-		val doc = getDocumentFromPage(aimURL)
+private val proString: () -> String = {
+	"""
+# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is getDocumentFromPage sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by getDocumentFromPage '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
 
-		println("The string is dealing...")
-		val host = doc.getElementById("host").attr("value")
+# localhost name resolution is handled within DNS itself.
+#	127.0.0.1       localhost
+#	::1             localhost
 
-		val ipTmp =
-			doc.getElementsByClass("w60-0 tl").text().split(" ".toRegex()).dropLastWhile { it.isEmpty() }
-				.toTypedArray()
-		val ip = arrayOfNulls<String>(ipTmp.size)
-		run {
-			var i = 0
-			var j = 0
-			while (i < ipTmp.size) {
-				ipTmp[i] = ipTmp[i].replace("[^\\d{1,3}.]".toRegex(), "").replace("\\.\\.+".toRegex(), "")
-				if (ipTmp[i] == "") {
-					i++
-					continue
-				}
-				ip[j] = ipTmp[i]
-				j++
-				i++
-			}
-		}
-
-//		val str = doc.getElementsByClass("w14-0").text().split(" ".toRegex()).dropLastWhile { it.isEmpty() }
-//			.toTypedArray()
-//		val ttl = arrayOfNulls<Int>(str.size)
-//		run {
-//			var i = 0
-//			var j = 0
-//			while (i < str.size) {
-//				if (str[i].contains("TTL")) {
-//					i++
-//					continue
-//				}
-//				ttl[j] = Integer.parseInt(str[i])
-//				j++
-//				i++
-//			}
-//		}
-
-
-		val tmp = Vector<String>()
-//		for (i in 0 until min(ttl.size, ip.size))
-//			if (ip[i] != null)
-//				if ((tmp.indexOf(ip[i]) == -1) and (ip[i] != "-"))
-//					tmp.addElement(ip[i] + " " + ttl[i])
-		for (s in ip) {
-			if (s != null)
-				if ((tmp.indexOf("$s $host") == -1) and (s != "-"))
-					tmp.addElement("$s $host")
-		}
-
-		tmp.sort()
-		for (i in tmp.indices)
-			recode.addElement("\n" + tmp.elementAt(i))
-	} catch (e: Exception) {
-		e.printStackTrace()
-	}
-
-	println("Finish")
-	recode.addElement("\n")
-	return recode
+	""".trimIndent()
 }
 
+private val openEtc: () -> Unit = { Desktop.getDesktop().open(File(path)) }
+
 private fun getDocumentFromPage(url: String): Document {
-	println("Page loading...")
 	//不打印日志
 	LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog")
 	java.util.logging.Logger.getLogger("com.gargoylesoftware").level = Level.OFF
@@ -149,41 +128,77 @@ private fun getDocumentFromPage(url: String): Document {
 	//等待后台运行
 	webClient.waitForBackgroundJavaScript((10 * 1000).toLong())
 
-	println("Loaded.")
 	return Jsoup.parse(page.asXml(), url)
 }
 
-private fun append(recode: Vector<String>): Boolean {
-	if (!recode.isEmpty() && backup()) {
-		val fileWriter = File("$DesktopPath\\hosts")
-		for (i in recode.indices)
-			fileWriter.appendText(recode.elementAt(i))
-		return true
-	}
-	return false
-}
+private fun readPage(url: String): Vector<String> {
+	val aimURL = "http://tool.chinaz.com/dns?type=1&host=$url&ip="
+	//设置代理
+//	System.setProperty("http.proxyHost", "127.0.0.1")
+//	System.setProperty("http.proxyPort", "8090")
 
-private fun updateHosts(urls: Vector<String>): Boolean {
-	if (!urls.isEmpty() && backup()) {
-		try {
-			val fileWriter = FileWriter("$DesktopPath\\hosts")
-			fileWriter.write(proString())
-			fileWriter.flush()
-			fileWriter.close()
+	val recode = Vector<String>()
 
-			for (i in urls.indices)
-				append(readPage(urls.elementAt(i)))
+	try {
+		val doc = getDocumentFromPage(aimURL)
 
-			//移动，但目前不能获取管理员权限写入C 盘
-			//				Files.move(bak1.toPath(), hosts.toPath());
+		val host = doc.getElementById("host").attr("value")
 
-			return true
-		} catch (e: IOException) {
-			e.printStackTrace()
+		val ipTmp =
+			doc.getElementsByClass("w60-0 tl").text().split("\\[.*?]".toRegex()).dropLastWhile { it.isEmpty() }
+				.toTypedArray()
+		val ip = arrayOfNulls<String>(ipTmp.size)
+		run {
+			var i = 0
+			var j = 0
+			while (i < ipTmp.size) {
+				ipTmp[i] = ipTmp[i].replace("([ \\-]|\\.\\.+)".toRegex(), "")
+				if (ipTmp[i] == "") {
+					i++
+					continue
+				}
+				ip[j++] = ipTmp[i++]
+			}
 		}
 
+//		val str = doc.getElementsByClass("w14-0").text().split(" ".toRegex()).dropLastWhile { it.isEmpty() }
+//			.toTypedArray()
+//		val ttl = arrayOfNulls<Int>(str.size)
+//		run {
+//			var i = 0
+//			var j = 0
+//			while (i < str.size) {
+//				if (str[i].contains("TTL")) {
+//					i++
+//					continue
+//				}
+//				ttl[j] = Integer.parseInt(str[i])
+//				j++
+//				i++
+//			}
+//		}
+
+
+//		val tmp = Vector<String>()
+//		for (i in 0 until min(ttl.size, ip.size))
+//			if (ip[i] != null)
+//				if ((tmp.indexOf(ip[i]) == -1) and (ip[i] != "-"))
+//					tmp.addElement(ip[i] + " " + ttl[i])
+
+		for (s in ip)
+			if (s != null)
+				if ((recode.indexOf("\n$s $host") == -1) and (s != "-"))
+					recode.addElement("\n$s $host")
+
+		recode.sort()
+//		for (i in tmp.indices)
+//			recode.addElement("\n" + tmp.elementAt(i))
+	} catch (e: Exception) {
+		e.printStackTrace()
 	}
-	return false
+
+	recode.addElement("\n")
+	return recode
 }
 
 private fun readHosts(): Vector<String>? {
@@ -195,7 +210,7 @@ private fun readHosts(): Vector<String>? {
 
 	val recode = Vector<String>()
 
-	val dirPath = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+	val dirPath = "$path\\hosts"
 	val fileReader = File(dirPath).bufferedReader()
 	var s = fileReader.readLine()
 	//逐行读取文件记录
@@ -217,39 +232,118 @@ private fun readHosts(): Vector<String>? {
 	return if (recode.isEmpty()) null else recode
 }
 
+private fun append(recode: Vector<String>) {
+	val fileWriter = File("$DesktopPath\\hosts")
+	for (i in recode.indices)
+		fileWriter.appendText(recode.elementAt(i))
+}
+
 fun menu() {
-	var s: String? = ""
+	var flag = true
 	//hosts 备份位于桌面
-	while (s != "quit") {
-		var flag: Boolean? = false
-		println("1 更新hosts (暂停服务)\n" + "2 新增URL\n" + "3 备份hosts\n" + "输入quit 退出")
-		s = readLine()
-		when (s) {
+	while (flag) {
+		flag = false
+		println("1 更新hosts\n" + "2 新增URL\n" + "3 备份hosts\t" + "输入quit 退出")
+		when (val s = readLine()) {
 			"1" -> {
-				flag = updateHosts(Objects.requireNonNull<Vector<String>>(readHosts()))
-				openEtc()
+				update()
 			}
 			"2" -> {
 				println("Input the URL:")
-				flag = readLine()?.let { readPage(it) }?.let { append(it) }
-				openEtc()
+				readLine()?.let { appendNew(it) }
 			}
 			"3" -> flag = backup()
+			else -> {
+				if (!s.equals("quit")) {
+					println("请重试")
+					flag = true
+				}
+			}
 		}
-		if (flag!!)
-			break
-		else
-			println("请重试")
 	}
 }
 
-fun toGUI() {
+class GUI internal constructor() : JFrame(), ActionListener {
+	private val updateHosts: JButton
+	private val search: JButton
+	private val backupHosts: JButton
+	private val hosts: JTextField
 
-}
+	init {
+		//左侧栏
+		val textArea = JTextArea("广告位招租")
+		//设置只读
+		textArea.isEditable = false
+		val update = JPanel()
+		update.add(textArea)
+		add(update, BorderLayout.WEST)
 
-fun main() {
-	menu()
-	toGUI()
-//	readHosts()
+		//中栏
+		val textA = JTextArea("到时候输出记录")
+		//设置只读
+		textA.isEditable = false
+		val recode = JPanel()
+		recode.layout = GridLayout(1, 1)
+		recode.add(textA)
+		add(recode, BorderLayout.CENTER)
 
+		//顶栏
+		hosts = JTextField()
+		search = JButton("搜索")
+		search.addActionListener(this)
+		val append = JPanel()
+		append.layout = GridLayout(1, 2)
+		append.add(hosts)
+		append.add(search)
+		add(append, BorderLayout.NORTH)
+
+		//底栏
+		backupHosts = JButton("备份")
+		backupHosts.addActionListener(this)
+		updateHosts = JButton("更新")
+		updateHosts.addActionListener(this)
+		val backup = JPanel()
+		backup.layout = GridLayout(1, 2)
+		backup.add(backupHosts)
+		backup.add(updateHosts)
+		add(backup, BorderLayout.SOUTH)
+
+		title = "test"
+		//大小
+		setSize(500, 500)
+		//是否可改变大小
+//		isResizable = false
+		//出现位置居中
+		setLocationRelativeTo(null)
+//		setLocation(1200, 200)
+		//关闭窗口按钮
+		defaultCloseOperation = EXIT_ON_CLOSE
+		//是否可见
+		isVisible = true
+	}
+
+	private fun setButtonStatus(f: Boolean, vararg a: JButton) {
+		for (button in a)
+			button.isEnabled = f
+	}
+
+	override fun actionPerformed(e: ActionEvent) {
+		when {
+			e.source === search -> {
+				setButtonStatus(false, backupHosts, updateHosts)
+				appendNew(hosts.text)
+				setButtonStatus(true, backupHosts, updateHosts)
+			}
+			e.source === backupHosts -> {
+				setButtonStatus(false, search, updateHosts)
+				backup()
+				setButtonStatus(true, search, updateHosts)
+			}
+			else -> {
+				setButtonStatus(false, search, backupHosts)
+				update()
+				setButtonStatus(true, search, backupHosts)
+			}
+		}
+	}
 }
